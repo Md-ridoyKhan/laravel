d5b1d745eb058ee8ff44c918cd61c24bb6810301
Rayhan Khan <ridoykhan.cse.web@gmail.com>
2022-03-28 16:16:24 +0600

    Laravel Installation

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Laravel installer</title>
+</head>
+<body>
+    
+    <h1>hi i am laravel installer</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum, alias corporis. Asperiores pariatur fugiat quidem eius nulla vero ipsa numquam quis quisquam nemo modi maiores quae, ad earum ratione voluptatibus.</p>
+
+</body>
+</html>
